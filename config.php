25810a418c4228d12536ff81f0d12b381bf0e7a5
Nick van Duijn <78408@b5-400b-32.ict.lab.locals>
2018-05-23 11:48:30 +0200

    test

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// database logingegevens
+$db_hostname = 'localhost';
+$db_username = 'ex78408';
+$db_password = 'iBE7GT';
+$db_database = 'dbex78408';
+
+// maak de database-verbinding
+$mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
+
